351d9478758e0f5bbcfcf661ab1bf675129f5865
Steven <2005013@iisigroup.com>
2022-09-02 14:26:19 +0800

    更新pybind位置

---
+++ Note.md
@@ -0,0 +1,5 @@
+# 安裝
+pip install .
+
+# 新增類別
+在src/scikit_build_example/__init__.py加入class的名稱
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -179,3 +179,54 @@ void Tracker::Run(const std::vector<cv::Rect>& detections) {
 std::map<int, Track> Tracker::GetTracks() {
     return tracks_;
 }
+
+
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+struct Pet {
+    Pet(const std::string &name) : name(name) { }
+    void setName(const std::string &name_) { name = name_; }
+    const std::string &getName() const { return name; }
+
+    std::string name;
+};
+
+PYBIND11_MODULE(_core, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: scikit_build_example
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &>());
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
