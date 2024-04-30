c839d01d3056ed3ddf05ef41c7652b9e0359b8c6
Steven <randy19962@gmail.com>
2024-04-30 14:28:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setup(
     include_package_data=True,
     extras_require={"test": ["pytest"]},
     python_requires=">=3.8",
-    cmake_args=['-G=Visual Studio 16 2019'] # cmake generator  https://cmake.org/cmake/help/v3.6/manual/cmake.1.html#options
+    #cmake_args=['-G=Visual Studio 16 2019'] # cmake generator  https://cmake.org/cmake/help/v3.6/manual/cmake.1.html#options
 )
